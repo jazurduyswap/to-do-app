5a9691c89b0663a52f03e4e82d39abc9a699a7d6
jazurduy <jazurduy@swapenergia.com>
2025-09-02 14:44:14 +0200

    feat: - Add new table Task

---
+++ src/Entity/Task.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TaskRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TaskRepository::class)]
+class Task
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $titulo = null;
+
+    #[ORM\ManyToOne(targetEntity: self::class, inversedBy: 'tasks')]
+    private ?self $padreTask = null;
+
+    /**
+     * @var Collection<int, self>
+     */
+    #[ORM\OneToMany(targetEntity: self::class, mappedBy: 'padreTask')]
+    private Collection $tasks;
+
+    public function __construct()
+    {
+        $this->tasks = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitulo(): ?string
+    {
+        return $this->titulo;
+    }
+
+    public function setTitulo(string $titulo): static
+    {
+        $this->titulo = $titulo;
+
+        return $this;
+    }
+
+    public function getPadreTask(): ?self
+    {
+        return $this->padreTask;
+    }
+
+    public function setPadreTask(?self $padreTask): static
+    {
+        $this->padreTask = $padreTask;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, self>
+     */
+    public function getTasks(): Collection
+    {
+        return $this->tasks;
+    }
+
+    public function addTask(self $task): static
+    {
+        if (!$this->tasks->contains($task)) {
+            $this->tasks->add($task);
+            $task->setPadreTask($this);
+        }
+
+        return $this;
+    }
+
+    public function removeTask(self $task): static
+    {
+        if ($this->tasks->removeElement($task)) {
+            // set the owning side to null (unless already changed)
+            if ($task->getPadreTask() === $this) {
+                $task->setPadreTask(null);
+            }
+        }
+
+        return $this;
+    }
+}
