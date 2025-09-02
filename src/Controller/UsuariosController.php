9189d4ca31e40f8c900b74041b271f27ac894149
jazurduy <jazurduy@swapenergia.com>
2025-09-02 08:19:09 +0200

    feat: - Usuarios refactorizacion de estructura y recomendaciones

--- src/Controller/UsuariosController.php
+++ src/Controller/UsuariosController.php
@@ -9,73 +9,107 @@ use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\UsuariosRepository;
 use App\Entity\Usuarios;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
-use Request;
+use Symfony\Component\HttpFoundation\Request;
 
 #[Route('/usuarios')]
 class UsuariosController extends AbstractController
 {   
     // Get All collection
-    #[Route('', methods: ['GET'])]
+    #[Route('', name: "usuarios_index", methods: ['GET'])]
     public function getCollection(UsuariosRepository $usuariosRepository): Response
     {
         $usuarios = $usuariosRepository->findAll();
-        return $this->render('main/usuarios.html.twig', [
+        return $this->render('usuarios/usuarios.html.twig', [
             'usuarios' => $usuarios
         ]);
     }
 
     // Get by ID
-    #[Route('/{id<\d+>}', methods: ['GET'])]
+    #[Route('/{id<\d+>}', name: "usuarios_mostrar", methods: ['GET'])]
     public function getById($id, UsuariosRepository $usuariosRepository): Response
     {
         $usuario = $usuariosRepository->findById($id);
         if (!$usuario) {
             throw $this->createNotFoundException('Usuario not found');
         }
-        return $this->render('main/usuario.html.twig', [
+        return $this->render('usuarios/usuario.html.twig', [
             'usuario' => $usuario
         ]);
     }
 
     // New User
-    #[Route('/nuevo', methods: ['GET'])]
+    #[Route('/nuevo', name: "usuarios_nuevo", methods: ['GET'])]
     public function newUser(): Response
     {
-        return $this->render('main/usuario_new.html.twig', []);
+        return $this->render('usuarios/usuario_new.html.twig', []);
     }
 
     // Edit By Id
-    #[Route('/editar/{id<\d+>}', methods: ['GET'])]
+    #[Route('/editar/{id<\d+>}', name: "usuarios_editar", methods: ['GET'])]
     public function editUser($id, UsuariosRepository $usuariosRepository): Response
     {
         $usuario = $usuariosRepository->findById($id);
         if (!$usuario) {
             throw $this->createNotFoundException('Usuario not found');
         }
-        return $this->render('main/usuario_edit.html.twig', [
+        return $this->render('usuarios/usuario_edit.html.twig', [
             'usuario' => $usuario
         ]);
     }
 
+    // Delete User
+    #[Route('/borrar/{id<\d+>}', name: "usuario_borrar", methods: ['POST'])]
+    public function delete($id, UsuariosRepository $usuariosRepository, ManagerRegistry $doctrine): Response
+    { 
+
+        $em = $doctrine->getManager();
+        $usuario = $usuariosRepository->findById($id);
+        if (!$usuario) {
+            throw $this->createNotFoundException('Usuario not found');
+        }
+        $em->remove($usuario);
+        $em->flush();
+        $this->addFlash('success', 'Usuario eliminado correctamente.');
+        return $this->redirectToRoute('usuarios_index');
+    }
+
     // Create a new User
-    #[Route('', methods: ['POST'])]
+    #[Route('', name: "usuario_crear", methods: ['POST'])]
     public function create(Request $request, ManagerRegistry $doctrine): Response
     {
         $em = $doctrine->getManager();
 
+        $id = trim((string) $request->request->get('id', ''));
         $nombre = trim((string) $request->request->get('nombre', ''));
         if ($nombre === '') {
             $this->addFlash('error', 'El nombre es obligatorio.');
             return $this->redirectToRoute('usuarios_new');
         }
 
-        $usuario = new Usuarios();
-        $usuario->setNombre($nombre);
+        // Crea Usuario
+        if(!$id || $id ==""){
+            $usuario = new Usuarios();
+            $usuario->setNombre($nombre);
 
-        $em->persist($usuario);
+            $em->persist($usuario);
+            $em->flush();
+            $this->addFlash('success', 'Usuario creado correctamente.');
+         } else {
+            // Editar usuario existente
+            $usuario = $em->getRepository(Usuarios::class)->find($id);
+
+            if (!$usuario) {
+                $this->addFlash('error', 'Usuario no encontrado.');
+                return $this->redirectToRoute('usuarios_index');
+            }
+
+            $usuario->setNombre($nombre);
+            $this->addFlash('success', 'Usuario actualizado correctamente.');
+        }
+
         $em->flush();
 
-        $this->addFlash('success', 'Usuario creado correctamente.');
         return $this->redirectToRoute('usuarios_index');
+        
     }
 }
