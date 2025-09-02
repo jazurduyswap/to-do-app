5704536bae84efe330c21657b50104ea9dc5871d
jazurduy <jazurduy@swapenergia.com>
2025-09-02 09:13:41 +0200

    feat: - Add field rol

--- src/Entity/Usuarios.php
+++ src/Entity/Usuarios.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\UsuariosRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: UsuariosRepository::class)]
@@ -15,6 +17,20 @@ class Usuarios
 
     #[ORM\Column(length: 255)]
     private ?string $nombre = null;
+
+    /**
+     * @var Collection<int, grupo>
+     */
+    #[ORM\ManyToMany(targetEntity: grupo::class, inversedBy: 'usuarios')]
+    private Collection $grupos;
+
+    #[ORM\Column(length: 150)]
+    private ?string $rol = null;
+
+    public function __construct()
+    {
+        $this->grupos = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -36,6 +52,42 @@ class Usuarios
     public function setNombre(string $nombre): static
     {
         $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, grupo>
+     */
+    public function getGrupos(): Collection
+    {
+        return $this->grupos;
+    }
+
+    public function addGrupo(grupo $grupo): static
+    {
+        if (!$this->grupos->contains($grupo)) {
+            $this->grupos->add($grupo);
+        }
+
+        return $this;
+    }
+
+    public function removeGrupo(grupo $grupo): static
+    {
+        $this->grupos->removeElement($grupo);
+
+        return $this;
+    }
+
+    public function getRol(): ?string
+    {
+        return $this->rol;
+    }
+
+    public function setRol(string $rol): static
+    {
+        $this->rol = $rol;
 
         return $this;
     }
