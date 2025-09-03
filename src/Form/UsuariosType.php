9d794a432075f7789fb7acb6eecbbd1668d6dabb
jazurduy <jazurduy@swapenergia.com>
2025-09-03 14:08:38 +0200

    refactor: - Show plain password on forms

--- src/Form/UsuariosType.php
+++ src/Form/UsuariosType.php
@@ -3,11 +3,14 @@
 namespace App\Form;
 
 use App\Entity\Grupo;
+use App\Form\GrupoType;
 use App\Entity\Usuarios;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class UsuariosType extends AbstractType
 {
@@ -15,13 +18,18 @@ class UsuariosType extends AbstractType
     {
         $builder
             ->add('nombre')
-            ->add('password')
+            ->add('plainPassword', PasswordType::class, [
+                'label' => 'Nueva contraseña',
+                'mapped' => false,
+                'required' => false,
+                'attr' => ['autocomplete' => 'new-password'],
+            ])
             ->add('email')
-            ->add('grupos', CollectionType::class, [
-                'entry_type' => GrupoType::class,
-                'allow_add' => true,
-                'allow_delete' => true,
-                'by_reference' => false,
+            ->add('grupos', EntityType::class, [
+                'class' => Grupo::class,
+                'choice_label' => 'nombre',
+                'multiple' => true,
+                'expanded' => true,
             ])
         ;
     }
